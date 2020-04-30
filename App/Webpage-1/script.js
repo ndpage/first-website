0acbb2ad4dc0f05e72591682772ba89b4814b2e8
Page Nathan (DC-MH/ENG1.13-NA) <nathan.page@bosch.com>
2020-04-30 12:03:32 -0400

    Added updatePrice() to script.js

--- App/Webpage-1/script.js
+++ App/Webpage-1/script.js
@@ -1,15 +1,17 @@
-$('.btn').click(function() {
-  
-    $('.text').text('loading . . .');
-    
-    $.ajax({
-      type:"GET",
-      url:"https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT",
-      success: function(data) {
-        //var priceData = JSON.stringify(data)
-          $('.text').append('<p>' + data.symbol + " $" + data.price + '</p>');
-        },
-      dataType: 'json',
-    });
-    
+
+
+
+
+
+function updatePrice(){
+  let currentdate = new Date();
+  $.ajax({
+    type:"GET",
+    url:"https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT",
+    success: function(data) {
+      //var priceData = JSON.stringify(data)
+        $('.text').append('<p>' + data.symbol + " $" + data.price +" as of " + currentdate + '</p>');
+      },
+    dataType: 'json',
   });
+}
